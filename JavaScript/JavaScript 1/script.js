d9724e4e13f03c1019b312908cb28f24fbd0d8ac
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-06-20 17:28:35 +0100

    feat: JavaScript Task

--- JavaScript/JavaScript 1/script.js
+++ JavaScript/JavaScript 1/script.js
@@ -1,4 +1,6 @@
 const firstName = 'Johnpaul';
 const lastName = 'Eyibra';
+const height = 174;
+const country = 'Nigeria';
 
-console.log(`My name is ${firstName} ${lastName}`);
+console.log(`My name is ${firstName} ${lastName}, I am ${height}cm tall and I live in ${country}`);
--- JavaScript/JavaScript 2/script.js
+++
@@ -1,6 +0,0 @@
-const firstName = 'Johnpaul';
-const lastName = 'Eyibra';
-const height = 174;
-const country = 'Nigeria';
-
-console.log(`My name is ${firstName} ${lastName}, I am ${height}cm tall and I live in ${country}`);
